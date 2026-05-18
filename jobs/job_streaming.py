84935be066753d78a9e0966674a247e91a903bd2
thediegoaccount <46111921+thediegoaccount@users.noreply.github.com>
2026-05-18 13:03:28 -0300

    refactor: remove redundant OAuth configuration for ADLS in streaming job

--- jobs/job_streaming.py
+++ jobs/job_streaming.py
@@ -31,15 +31,7 @@ def main():
     client_secret = dbutils.secrets.get(scope="kv-case-santander", key="client-secret")
     storage_account = dbutils.secrets.get(scope="kv-case-santander", key="storage-account")
 
-    # CONFIGURAR ADLS IMEDIATAMENTE
     configure_adls(spark, storage_account, client_id, tenant_id, client_secret)
-    
-    # Adicionar configs OAuth extras (redundância pra garantir)
-    spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "OAuth")
-    spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-    spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account}.dfs.core.windows.net", client_id)
-    spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account}.dfs.core.windows.net", client_secret)
-    spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
 
     bronze_kafka_path = f"abfss://bronze@{storage_account}.dfs.core.windows.net/kafka/"
     silver_streaming_path = f"abfss://silver@{storage_account}.dfs.core.windows.net/streaming/"
